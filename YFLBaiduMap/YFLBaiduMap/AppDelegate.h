3f7bbf4f1d90b24ea4ca821eeedee326ce07720f
xiaoyang <xiaoyang@maihaoche.com>
2016-11-09 18:23:48 +0800

    导入百度mapsdk

---
+++ YFLBaiduMap/YFLBaiduMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YFLBaiduMap
+//
+//  Created by 杨丰林 on 16/11/9.
+//  Copyright © 2016年 杨丰林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YFLBaiduMap/YFLBaiduMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YFLBaiduMap
+//
+//  Created by 杨丰林 on 16/11/9.
+//  Copyright © 2016年 杨丰林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
